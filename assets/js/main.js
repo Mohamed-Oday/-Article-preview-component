3c3335486b2b53da616c051653b4536722db0bef
Mohamed-Oday <160322375+Mohamed-Oday@users.noreply.github.com>
2024-07-17 18:22:06 -0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,11 @@ shareBtn.addEventListener('click', () => {
     if (shareBtnSocials.classList.contains('hidden') && shareArrow.classList.contains('hidden')){
         shareBtnSocials.classList.remove('hidden')
         shareArrow.classList.remove('hidden')
+        shareBtn.classList.add('mt-[30px]')
     }
     else{
         shareBtnSocials.classList.add('hidden')
         shareArrow.classList.add('hidden')
+        shareBtn.classList.remove('mt-[30px]')
     }
 })
